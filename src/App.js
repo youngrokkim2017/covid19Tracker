ebd1adf8ef3d303939cec04b68ea10427a017cb2
Patrick Kim <patrick.yrkim@gmail.com>
2021-09-07 23:25:30 -0700

    Add components

--- src/App.js
+++ src/App.js
@@ -1,37 +1,13 @@
 import React from 'react';
-import {
-  MenuItem,
-  FormControl,
-  Select,
-} from '@material-ui/core'
 
-function App() {
-  return (
-    <div className="app">
-      <h1>COVID 19 Tracker</h1>
-
-      <FormControl>
-        <Select
-          variant="outlined"
-          value="abc"
-        >
-          <MenuItem value="worldwide">Worldwide</MenuItem>
-        </Select>
-      </FormControl>
-
-      {/* header */}
-      {/* title and dropdown menu */}
-
-      {/* infobox */}
-      {/* infobox */}
-      {/* infobox */}
-
-      {/* table */}
-      {/* graph */}
-
-      {/* map */}
-    </div>
-  );
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>App</h1>
+            </div>
+        )
+    }
 }
 
 export default App;
