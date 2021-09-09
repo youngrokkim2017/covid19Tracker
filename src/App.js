0f47b35501af8b8540a289a9d1ae6b90de8cf6d4
Patrick Kim <patrick.yrkim@gmail.com>
2021-09-09 15:58:44 -0700

    Add function to fetch data

--- src/App.js
+++ src/App.js
@@ -2,12 +2,27 @@ import React from 'react';
 import Cards from ' ./components/Cards/Cards';
 import Chart from ' ./components/Chart/Chart';
 import CountryPicker from ' ./components/CountryPicker/CountryPicker';
+import { fetchData } from './api/index'
 
 class App extends React.Component {
+    state = {
+        data: {}.
+    }
+
+    componentDidMount() {
+        const fetchedData = await fetchData();
+
+        // console.log(data);
+
+        this.setState({ data: fetchedData });
+    }
+
     render() {
+        const { data } = this.state;
+
         return (
             <div>
-                <Cards />
+                <Cards data={data} />
                 <CountryPicker />
                 <Chart />
             </div>
