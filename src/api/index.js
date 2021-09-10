78e17cba531bacc03355f9591036784339fb3332
Patrick Kim <patrick.yrkim@gmail.com>
2021-09-09 19:15:05 -0700

    Update chart data

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 const url = 'https://covid19.mathdro.id/api';
 
-const fetchData = async () => {
+export const fetchData = async () => {
     try {
         const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(url);
 
@@ -15,5 +15,21 @@ const fetchData = async () => {
         return modifiedData;
     } catch (err) {
         console.log(err);
+    }
+}
+
+export const fetchDailyData = async () => {
+    try {
+        const { data } = await axios.get(`${url}/daily`)
+
+        const modifiedData = data.map((dailyData) => ({
+            confirmed: dailyData.confirmed.total,
+            deaths: dailyData.deaths.total,
+            date: dailyData.reportDate,
+        }))
+
+        return modifiedData;
+    } catch (err) {
+        console.log(err)
     }
 }
--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,8 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { fetchDailyData } from '../../api';
+import { Line, Bar } from 'react-chartjs-2';
 
 const Chart = () => {
+    const [dailyData, setDailyData] = useState([]);
+
+    useEffect(() => {
+        const fetchAPI = async () => {
+            setDailyData(await fetchDailyData());
+        }
+
+        fetchAPI();
+    });
+
+    const lineChart = (
+        dailyData.length
+            ? (
+            <Line
+                data={{
+                    labels: dailyData.map(({ date }) => date),
+                    datasets: [
+                        { 
+                            data: dailyData.map(({ confirmed }) => confirmed),
+                            label: "Infected"
+                        }, 
+                        {
+                            data: dailyData.map(({ deaths }) => deaths),
+                            label: "Deaths"
+                        },
+                    ],
+                }}
+            />
+            )
+            :
+            null
+    )
+
     return (
-        <h1>Chart</h1>
+        <div>
+            {lineChart}
+        </div>
     )
 }
 
